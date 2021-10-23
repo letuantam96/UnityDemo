d39456686152e055995b51d62cd1b54f6530467a
letuantam <letuantam96@gmail.com>
2021-10-23 16:45:33 +0700

    add Light map to scene

---
+++ Assets/Scene4_Controller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene4_Controller : MonoBehaviour
+{
+    public void BackBtnPressed()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
